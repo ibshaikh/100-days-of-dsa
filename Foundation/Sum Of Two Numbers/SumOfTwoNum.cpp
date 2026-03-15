9806c899228a9a099b9dd241ab09545fb642fc06
Ibram <shaikhibuu07@gmail.com>
2026-03-15 18:13:24 +0530

    Added Sum Of Two Numbers Problem

---
+++ Foundation/Sum Of Two Numbers/SumOfTwoNum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void sum(int a, int b){
+    int add = a + b;
+    cout << add << endl;
+}
+
+int main(){
+    sum(7,5);
+    return 0;
+}
---
+++ Foundation/Sum Of Two Numbers/SumOfTwoNum.js
@@ -0,0 +1,16 @@
+/* Write a Program that defines a function to
+   calculate the sum of two integers and prints the result.
+   Call this function by passing two integer values. */
+
+
+/* Approach -
+    1. Define a function that takes two numbers as a input.
+    2. Add the two numbers inside the function.
+    3. Call the function with two numbers And Print the result. */
+
+function sum(a,b){
+    let add = a + b;
+    console.log(add);  
+}
+
+sum(7,7);

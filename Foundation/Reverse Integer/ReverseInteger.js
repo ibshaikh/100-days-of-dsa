0e32836d4257a2789e48f15073ccaee4368c934a
Ibram <shaikhibuu07@gmail.com>
2026-03-18 23:52:31 +0530

    Added Reverse Integer Problem Solution

---
+++ Foundation/Reverse Integer/ReverseInteger.js
@@ -0,0 +1,14 @@
+var reverse = function(x) {
+  let xCopy = x;
+  x = Math.abs(x);
+  let rev = 0;
+  while (x > 0) {
+    let last = x % 10;
+    rev = rev * 10 + last;
+    x = Math.floor(x / 10);
+  }
+  if (rev > 2**31 - 1) return 0;
+  return xCopy < 0 ? -rev : rev;
+};
+
+console.log(reverse(123));

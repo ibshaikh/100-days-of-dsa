68df81b2d2481e45a4e50f4fe382600de23bd808
Ibram <shaikhibuu07@gmail.com>
2026-04-04 19:09:37 +0530

    Strings - Reverse String II Problem And Solution Added

---
+++ Strings/Reverse String II/ReverseStringII.cpp
@@ -0,0 +1,13 @@
+  #include <iostream>
+  #include <string>
+  #include <algorithm>
+  using namespace std;
+  
+  string reverseStr(string s, int k) {
+      for (int i = 0; i < s.length(); i += 2 * k) {
+          int end = min(i + k, (int)s.length());
+          reverse(s.begin() + i, s.begin() + end);
+      }
+      return s;
+  }
+      

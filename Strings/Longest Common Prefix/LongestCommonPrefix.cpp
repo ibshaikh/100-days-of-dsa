24ddb53a6b673b08c49548e490c31b79fd6babd0
Ibram <shaikhibuu07@gmail.com>
2026-04-05 16:37:49 +0530

    Strings - Longest Common Prefix problem and solution added

---
+++ Strings/Longest Common Prefix/LongestCommonPrefix.cpp
@@ -0,0 +1,18 @@
+  #include <iostream>
+  #include <vector>
+  using namespace std;
+  
+  string longestCommonPrefix(vector<string>& strs) {
+      int x = 0;
+      while (x < strs[0].length()) {
+          char ch = strs[0][x];
+          for (int i = 1; i < strs.size(); i++) {
+              if (x == strs[i].length() || ch != strs[i][x]) {
+                  return strs[0].substr(0, x);
+              }
+          }
+          ++x;
+      }
+      return strs[0];
+  }
+    

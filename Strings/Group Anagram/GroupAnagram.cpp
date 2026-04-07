b084a7973998949a71934cb98d9b82064bda8f5a
Ibram <shaikhibuu07@gmail.com>
2026-04-07 23:59:13 +0530

    Strings - Group Anagram problem and c++ solution added And Stack And Queue Problem Implement Stack

---
+++ Strings/Group Anagram/GroupAnagram.cpp
@@ -0,0 +1,23 @@
+  #include <iostream>
+  #include <vector>
+  #include <unordered_map>
+  #include <algorithm>
+  using namespace std;
+  
+  vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      unordered_map<string, vector<string>> map;
+  
+      for (string str : strs) {
+          string key = str;
+          sort(key.begin(), key.end());
+          map[key].push_back(str);
+      }
+  
+      vector<vector<string>> result;
+      for (auto entry : map) {
+          result.push_back(entry.second);
+      }
+  
+      return result;
+  }
+  

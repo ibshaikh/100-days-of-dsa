94d824d5b12c46ef8f48f1e733a7111a2be58684
Ibram <shaikhibuu07@gmail.com>
2026-04-08 21:49:05 +0530

    Stack And Queues - Valid Parenthesis Solution Added C++

---
+++ Stack And Queues/Valid Parentheses/ValidParentheses.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+bool isValid(const std::string &s) {
+    std::vector stack;
+    std::unordered_map map = {
+        {'{', '}'},
+        {'[', ']'},
+        {'(', ')'}
+    };
+
+    for (char c : s) {
+        if (map.count(c)) {
+            stack.push_back(c);
+        } else {
+            if (stack.empty()) return false;
+            char top = stack.back();
+            stack.pop_back();
+            if (c != map[top]) return false;
+        }
+    }
+    return stack.empty();
+}

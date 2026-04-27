3848a12f30d1929f601cc194616ea59218e18f80
Ibram <shaikhibuu07@gmail.com>
2026-04-27 20:13:00 +0530

    Two Pointers & Sliding Window - Two Sum's Solution Added - JS

---
+++ Two Pointers & Sliding Window/Two Sum/TwoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+    let n = nums.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let sum = nums[i] + nums[j];
+            if (sum === target) {
+                return [i, j];
+            }
+        }
+    }
+} 

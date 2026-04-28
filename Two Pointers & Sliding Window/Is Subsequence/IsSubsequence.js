97f3b6841bf3b0fbeb49e6f7c72599d8fd1b0404
Ibram <shaikhibuu07@gmail.com>
2026-04-28 22:58:12 +0530

    Two Pointers & Sliding Window - Is Subsequence's Solution Added - JS

---
+++ Two Pointers & Sliding Window/Is Subsequence/IsSubsequence.js
@@ -0,0 +1,10 @@
+var isSubsequence = function(s, t) {
+    let i = j = 0;
+    while(j < t.length) {
+        if(s[i] === t[j]) {
+            ++i
+        } 
+        ++j;
+    }
+    return i === s.length;
+};

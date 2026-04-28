7c8bba648e121b6a98d56991d41672467d5bd198
Ibram <shaikhibuu07@gmail.com>
2026-04-28 23:01:06 +0530

    Two Pointers & Sliding Window - Is Subsequence's Solution Added - CPP

---
+++ Two Pointers & Sliding Window/Is Subsequence/IsSubsequence.cpp
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+bool isSubsequence(string s, string t) {
+    int i = 0, j = 0;
+    while (j < t.length()) {
+        if (s[i] == t[j]) {
+            ++i;
+        }
+        ++j;
+    }
+    return i == s.length();
+}

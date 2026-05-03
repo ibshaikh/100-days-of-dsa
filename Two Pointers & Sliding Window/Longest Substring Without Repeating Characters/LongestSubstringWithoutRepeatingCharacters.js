5a1ac441cfd01efa5f4381108cc3e068c27b4917
Ibram <shaikhibuu07@gmail.com>
2026-05-03 19:04:14 +0530

    Two Pointers & Sliding Window - Longest Substring Without Repeating Characters's Solution Added - JS

---
+++ Two Pointers & Sliding Window/Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,18 @@
+var lengthOfLongestSubstring = function(s) {
+    let i = 0, j = 0;
+    let map = {};
+    let maxWS = 0;
+
+    for (j = 0; j < s.length; j++) {
+        if (map[s[j]] !== undefined && map[s[j]] >= i) {
+            i = map[s[j]] + 1;
+        }
+
+        map[s[j]] = j;
+
+        let currWS = j - i + 1;
+        maxWS = Math.max(maxWS, currWS);
+    }
+
+    return maxWS;
+};
